6710ed7f8f70fc2643d1f3118df619d076746680
Alex de Campos Santos Toledo Camilo <alexcs@outlook.com.br>
2022-08-22 11:50:13 -0300

    Initial commit

---
+++ Aprendendo.py
@@ -0,0 +1,10 @@
+a = -1
+b = -2
+
+if b > a:
+        if b > 0:
+                print "b é maior que a\nb é positivo"
+            else:
+                print "b não é maior que a nem positivo"
+            else:
+                print "b menor que a"
